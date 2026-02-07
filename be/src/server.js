8ad41862c24b119205394f0a42385a174584ed1c
Siddhesh <siddheshpatil019@gmail.com>
2026-02-08 00:29:12 +0530

    bundled ui into backend to deploy as one app

--- be/src/server.js
+++ be/src/server.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -9,10 +10,20 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const port = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../ui/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../ui", "dist", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`server running on port: ${port}`));

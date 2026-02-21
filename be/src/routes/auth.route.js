36a51746f60b441c53d94dc87af6f5c66ff84553
Siddhesh <siddheshpatil019@gmail.com>
2026-02-21 12:18:29 +0530

    added rate limiting and bot detection check with arcjet

--- be/src/routes/auth.route.js
+++ be/src/routes/auth.route.js
@@ -6,8 +6,11 @@ import {
   updateProfile,
 } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 
@@ -18,7 +21,7 @@ router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateProfile);
 
 router.get("/check", protectRoute, (req, res) =>
-  res.status(200).json(req.user)
+  res.status(200).json(req.user),
 );
 
 export default router;
